8bb711934f27df27839ab29f3733896b53a56726
Andjelko28 <105885932+Andjelko28@users.noreply.github.com>
2024-06-15 18:11:38 +0200

    "Removed Gate authorization in JobController, updated routes in web.php to use controller grouping and added middleware to job routes"

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 
 use App\Http\Controllers\JobController;
 use App\Http\Controllers\RegisterUserController;
-use App\Http\Controllers\LoginUsercontroller;
+use App\Http\Controllers\LoginUserController;
 
 use Illuminate\Support\Facades\Route;
 
@@ -13,23 +13,21 @@ Route::view('/', 'home');
 Route::view('contact', 'contact');
 
 // Group same routes
-// Route::controller(JobController::class)->group(function () {
-//     Route::get('/jobs', ['index']);
-//     Route::get('/jobs/create', ['create']);
-//     Route::get('/jobs/{job}', ['show']);
-//     Route::post('jobs', ['store']);
-//     Route::get('/jobs/{job}/edit', ['edit']);
-//     Route::patch('/jobs/{job}', ['update']);
-//     Route::delete('/jobs/{job}', ['destroy']);
-//});
-
-Route::resource('jobs', JobController::class);
+Route::controller(JobController::class)->group(function () {
+    Route::get('/jobs', ['index']);
+    Route::get('/jobs/create', ['create']);
+    Route::get('/jobs/{job}', ['show']);
+    Route::post('jobs', ['store'])->middleware('auth');
+    Route::get('/jobs/{job}/edit', ['edit'])->middleware(['auth', 'can:edit-job,job']);
+    Route::patch('/jobs/{job}', ['update']);
+    Route::delete('/jobs/{job}', ['destroy']);
+});
 
 
 // Auth
 Route::get('/register', [RegisterUserController::class, 'create']);
 Route::post('/register', [RegisterUserController::class, 'store']);
 
-Route::get('/login', [LoginUserController::class, 'create']);
-Route::post('/login', [LoginUsercontroller::class, 'store']);
-Route::post('/logout', [LoginUsercontroller::class, 'destroy']);
+Route::get('/login', [LoginUserController::class, 'create'])->name('login');
+Route::post('/login', [LoginUserController::class, 'store']);
+Route::post('/logout', [LoginUserController::class, 'destroy']);
