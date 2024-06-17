aada3fcdaab5646350250c7596494847227ad96d
Andjelko28 <105885932+Andjelko28@users.noreply.github.com>
2024-06-17 11:10:41 +0200

    'Include vite and import tailwind css for stylesheet

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -7,7 +7,7 @@
         @foreach($jobs as $job)
             <a href="/jobs/{{$job['id']}}" class="block px4 py-6 border borde-gray-200 rounder-lg">
                 <div>
-                    <strong>{{$job['title']}}:</strong> Pays {{$job['salary']}} per year
+                    <strong class="text-laracasts">{{$job['title']}}:</strong> Pays {{$job['salary']}} per year
                 </div>
             </a>
         @endforeach
